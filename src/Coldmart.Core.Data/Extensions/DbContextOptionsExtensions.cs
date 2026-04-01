b73d1ea57a8f3f152fa329a19e6f1a1724e8237e
Gilberto <Gilberto@DESKTOP-MSF9TJN>
2026-04-01 20:06:13 -0300

    Separando DBs por serviço

--- src/Coldmart.Core.Data/Extensions/DbContextOptionsExtensions.cs
+++ src/Coldmart.Core.Data/Extensions/DbContextOptionsExtensions.cs
@@ -1,18 +1,27 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Coldmart.Core.Data.Extensions;
 
 [ExcludeFromCodeCoverage]
 public static class DbContextOptionsExtensions
 {
-    public static void ConfigureDbContextOptions(this DbContextOptionsBuilder options, IConfiguration configuration)
+    public static void ConfigureDbContextOptions(
+        this DbContextOptionsBuilder options,
+        IConfiguration configuration,
+        IHostEnvironment environment)
     {
-        // O ideal não seria usar lazy loading para todos os contextos da aplicação, apenas onde é necessário.
-        // Decidi acidionar em todos para simplificar o desenvolvimento neste projeto.
-        options
-            .UseLazyLoadingProxies()
-            .UseSqlServer(configuration.GetConnectionString("ColdmartDb"));
+        options.UseLazyLoadingProxies();
+
+        if (environment.IsDevelopment())
+        {
+            options.UseSqlite(configuration.GetConnectionString("ColdmartDb"));
+        }
+        else
+        {
+            options.UseSqlServer(configuration.GetConnectionString("ColdmartDb"));
+        }
     }
 }
